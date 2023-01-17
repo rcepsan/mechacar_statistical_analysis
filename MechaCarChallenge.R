8ccffd717d6ee4abf92e872e199d83f70aa49b11
Roselyanne Cepero Santos <roselyannecepero@gmail.com>
2023-01-16 20:43:41 -0500

    Uploading Deliverable 3

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -15,3 +15,13 @@ lot_summary <- suspension_coil_table %>%
   group_by(Manufacturing_Lot) %>%
   summarize(mean = mean(PSI), median = median(PSI), variance = var(PSI), std_dev = sd(PSI))
 print(lot_summary)
+
+result <- t.test(suspension_coil_table$PSI, mu = 1500)
+print(result)
+
+result_1 <- t.test(subset(suspension_coil_table, Manufacturing_Lot == "Lot1")$PSI, mu = 1500)
+result_2 <- t.test(subset(suspension_coil_table, Manufacturing_Lot == "Lot2")$PSI, mu = 1500)
+result_3 <- t.test(subset(suspension_coil_table, Manufacturing_Lot == "Lot3")$PSI, mu = 1500)
+print(result_1)
+print(result_2)
+print(result_3)
